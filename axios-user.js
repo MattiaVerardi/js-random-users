86bf150b79d7a19495f727b7c013cef9bde04c12
MattiaVerardi <verardi.mattia97@gmail.com>
2022-08-03 17:10:54 +0200

    consegna base

--- axios-user.js
+++ axios-user.js
@@ -4,8 +4,8 @@ addButton.addEventListener("click", addUser);
 function addUser() {
   axios
     .get("https://randomuser.me/api")
-    .then(function (responde) {
-      const user = response.data.results;
+    .then(function (response) {
+      const user = response.data.results[0];
       createUser(user);
     })
     .catch(function (error) {
@@ -13,4 +13,17 @@ function addUser() {
     });
 }
 
-function createUser() {}
+function createUser(user) {
+  let image = document.createElement("img");
+  image.src = user.picture.large;
+  let firstName = document.createElement("h3");
+  firstName.innerText = user.name.first;
+  let lastName = document.createElement("h3");
+  lastName.innerText = user.name.last;
+  let div = document.createElement("div");
+  div.appendChild(image);
+  div.appendChild(firstName);
+  div.appendChild(lastName);
+  div.className = "col-2 mb-3";
+  document.getElementById("gallery").appendChild(div);
+}
